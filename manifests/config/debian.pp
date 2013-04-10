0518e1beaf69698a4f5d3dafccafc1a1a01bcb77
Udo Waechter <u.waechter@mysportgroup.de>
2013-04-10 12:23:01 +0200

    fixing download paths for pam.d/ in debian

--- manifests/config/debian.pp
+++ manifests/config/debian.pp
@@ -1,45 +1,35 @@
 
 class pam::config::debian {
-	
-	include pam::params
-	
-	#augeas { "${pam::params::prefix}/":
-	#	context => "/files/${pam::params::prefix} ... ",
-	#	changes => $databases,
-	#}
+  include pam::params
 
-	File {
-		ensure  => present,
-		owner   => 'root',
-		group   => 'root',
-		mode    => 0644,
-		require => Package[$pam::params::package],
-	}
+  File {
+    ensure  => present,
+    owner   => 'root',
+    group   => 'root',
+    mode    => 0644,
+    require => Package[$pam::params::package],
+  }
 
-	file { '/etc/pam_ldap.conf':
-		ensure  => link,
-		target  => '/etc/ldap/ldap.conf',
-	}
+  file {
+    '/etc/pam_ldap.conf':
+      ensure => link,
+      target => '/etc/ldap/ldap.conf';
 
-	file { '/etc/pam.d/common-account':
-		source  => "puppet:///modules/pam/pam.d/common-account",
-	}
+    '/etc/pam.d/common-account':
+      source => "puppet:///modules/pam/debian/etc/pam.d/common-account";
 
-	file { '/etc/pam.d/common-auth':
-		source  => "puppet:///modules/pam/pam.d/common-auth",
-	}
+    '/etc/pam.d/common-auth':
+      source => "puppet:///modules/pam/debian/etc/pam.d/common-auth";
 
-	file { '/etc/pam.d/common-password':
-		source  => "puppet:///modules/pam/pam.d/common-password",
-	}
+    '/etc/pam.d/common-password':
+      source => "puppet:///modules/pam/debian/etc/pam.d/common-password";
 
-	file { '/etc/pam.d/common-session':
-		source  => "puppet:///modules/pam/pam.d/common-session",
-	}
+    '/etc/pam.d/common-session':
+      source => "puppet:///modules/pam/debian/etc/pam.d/common-session";
 
-	file { '/etc/pam.d/common-session-noninteractive':
-		source  => "puppet:///modules/pam/pam.d/common-session-noninteractive",
-	}
+    '/etc/pam.d/common-session-noninteractive':
+      source => "puppet:///modules/pam/debian/etc/pam.d/common-session-noninteractive";
+  }
 
 }
 
