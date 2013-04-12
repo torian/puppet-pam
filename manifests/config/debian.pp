3490e78540cfab8c1486c8c6a4a78763d3da1467
Emiliano Castagnari <ecastag@gmail.com>
2013-04-12 07:49:46 -0300

    Merged zoide modifications. Added support for optional inclusion of module pam_mkhomedir on common-session/-noninteractive (and moved file to template). Got back to multiple file resources (insetead of one). It is less error prone.

--- manifests/config/debian.pp
+++ manifests/config/debian.pp
@@ -1,35 +1,45 @@
 
 class pam::config::debian {
-  include pam::params
-
-  File {
-    ensure  => present,
-    owner   => 'root',
-    group   => 'root',
-    mode    => 0644,
-    require => Package[$pam::params::package],
-  }
-
-  file {
-    '/etc/pam_ldap.conf':
-      ensure => link,
-      target => '/etc/ldap/ldap.conf';
-
-    '/etc/pam.d/common-account':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-account";
-
-    '/etc/pam.d/common-auth':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-auth";
-
-    '/etc/pam.d/common-password':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-password";
-
-    '/etc/pam.d/common-session':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-session";
-
-    '/etc/pam.d/common-session-noninteractive':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-session-noninteractive";
-  }
-
+	
+	include pam::params
+	
+	File {
+		ensure  => present,
+		owner   => 'root',
+		group   => 'root',
+		mode    => 0644,
+		require => Package[$pam::params::package],
+	}
+	
+	file { '/etc/pam_ldap.conf':
+		ensure => link,
+		target => '/etc/ldap/ldap.conf';
+	}
+	
+	file { '/etc/pam.d/common-account':
+		source  => "puppet:///modules/pam/debian/etc/pam.d/common-account",
+		require => File['/etc/pam_ldap.conf'],
+	}
+	
+	file { '/etc/pam.d/common-auth':
+		source => "puppet:///modules/pam/debian/etc/pam.d/common-auth",
+		require => File['/etc/pam_ldap.conf'],
+	}
+	
+	file { '/etc/pam.d/common-password':
+		source => "puppet:///modules/pam/debian/etc/pam.d/common-password",
+		require => File['/etc/pam_ldap.conf'],
+	}
+	
+	file { '/etc/pam.d/common-session-noninteractive':
+		source => "puppet:///modules/pam/debian/etc/pam.d/common-session-noninteractive",
+		require => File['/etc/pam_ldap.conf'],
+	}
+	
+	file { '/etc/pam.d/common-session':
+		content => template("pam.d/debian/common-session.erb"),
+		require => File['/etc/pam_ldap.conf'],
+	}
+	
 }
 
