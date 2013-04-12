88a45be578cfbe0bdf7619c0be2cf34d7d693aa6
Emiliano Castagnari <ecastag@gmail.com>
2013-04-12 07:54:04 -0300

    Added support for pam_mkhomedir

--- manifests/init.pp
+++ manifests/init.pp
@@ -4,7 +4,9 @@
 #   - RHEL   5.2   / 5.4   / 5.5   / 6.1   / 6.2 /
 #   - OVS    2.1.1 / 2.1.5 / 2.2.0 / 3.0.2 /
 
-class pam($module_type = 'none', $ensure = present) {
+class pam($module_type = 'none',
+	$pam_mkhomedir = false,
+	$ensure        = present) {
 
 	include pam::params
 	include pam::package
