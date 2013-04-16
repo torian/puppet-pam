725955e34ac8fa9fa98502eda5cbb307aeb0da30
Udo Wächter <u.waechter@mysportgroup.de>
2013-04-16 11:21:44 +0200

    making pam_mkhomedir optional. Use it as: '::params::pam_mkhomedir = "umask=0022 skel=/etc/skel"'

--- manifests/config/debian.pp
+++ manifests/config/debian.pp
@@ -10,25 +10,27 @@ class pam::config::debian {
     require => Package[$pam::params::package],
   }
 
+  $b_url = 'pam/debian/etc/pam.d'
+
   file {
     '/etc/pam_ldap.conf':
       ensure => link,
       target => '/etc/ldap/ldap.conf';
 
     '/etc/pam.d/common-account':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-account";
+      source => template("${b_url}/common-account.erb");
 
     '/etc/pam.d/common-auth':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-auth";
+      source => template("${b_url}/common-auth.erb");
 
     '/etc/pam.d/common-password':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-password";
+      source => template("${b_url}/common-password.erb");
 
     '/etc/pam.d/common-session':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-session";
+      source => template("${b_url}/common-session.erb");
 
     '/etc/pam.d/common-session-noninteractive':
-      source => "puppet:///modules/pam/debian/etc/pam.d/common-session-noninteractive";
+      source => template("${b_url}/common-session-noninteractive.erb");
   }
 
 }
