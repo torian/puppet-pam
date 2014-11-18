7f0eebab78b893482ae0a045b4843695ca96c41b
Matthew Schmitt <mschmitt@sugarsync.com>
2014-11-18 14:59:22 -0800

    Adding new template and updating existing template. Updated redhat.pp to manage new template

--- manifests/pamd/redhat.pp
+++ manifests/pamd/redhat.pp
@@ -6,7 +6,6 @@ class pam::pamd::redhat {
 
   File {
     ensure => present,
-    links  => follow,
     owner  => 'root',
     group  => 'root',
     mode   => '0644'
@@ -17,10 +16,21 @@ class pam::pamd::redhat {
   }
 
   file { "${pam::params::prefix_pamd}/system-auth":
-    content => template('pam/pam.d/system-auth-ac.erb')
+    ensure => 'link',
+    target => '/etc/pam.d/system-auth-ac'
+  }
+
+  file { "${pam::params::prefix_pamd}/smartcard-auth-ac":
+    content => template('pam/pam.d/smartcard-auth-ac.erb')
+  }
+
+  file { "${pam::params::prefix_pamd}/smartcard-auth":
+    ensure => 'link',
+    target => '/etc/pam.d/smartcard-auth-ac'
   }
 
   if($pam::pamd::pam_ldap) {
+
     #File['/etc/ldap.conf'] -> File[$pam::params::ldap_conf]
 
     file { '/etc/ldap.conf':
@@ -35,12 +45,15 @@ class pam::pamd::redhat {
     5 : {}
 
     6 : {
+
       file { "${pam::params::prefix_pamd}/password-auth-ac":
         ensure  => present,
         content => template('pam/pam.d/system-auth-ac.erb'),
       }
+
       # rhel 6 uses pam_ldap.conf:
       if($pam::pamd::pam_ldap) {
+
         #File['/etc/pam_ldap.conf'] -> File[$pam::params::ldap_conf]
 
         file { '/etc/pam_ldap.conf':
@@ -49,13 +62,13 @@ class pam::pamd::redhat {
         }
 
       }
+
     }
 
     default : {
-      notice("Version ${::operatingsystemmajrelease} not handled")
+      notice("Version $::operatingsystemmajrelease not handled")
     }
 
   }
 
 }
-
